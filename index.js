552e28011f063fa2a686051bd172536bf5075458
kushan2k <kushangayantha001@gmail.com>
2023-08-10 12:35:05 +0530

    Added:google place details api tested

--- index.js
+++ index.js
@@ -11,17 +11,35 @@ const BASE_URL_NEARBY_SEARCH =
 //to get open now places use open
 const url = `${BASE_URL_NEARBY_SEARCH}location=${process.env.MY_LATITUDE},${process.env.MY_LOGITUTE}&radius=50000&key=${process.env.GOOGLE_API_KEY}`
 
-axios({ method: "get", url: url })
+// axios({ method: "get", url: url })
+//   .then((res) => {
+//     res.data.results.map((place) => {
+//       console.log("name ", place.name)
+//       console.log("id ", place.place_id)
+//       if (place.photos) {
+//         place.photos.map((pic) => console.log(pic))
+//       }
+//       console.log("Geometry ", place.geometry)
+//       console.log("\n==============================")
+//     })
+//   })
+//   .catch((er) => {
+//     console.log(er)
+//   })
+
+//get place details api
+const PLACE_DETAILS_BASE_URL =
+  "https://maps.googleapis.com/maps/api/place/details/json?"
+const place_id = "ChIJlzgTfR464zoR_SLRysBXfJw"
+
+const detilas_url = `${PLACE_DETAILS_BASE_URL}place_id=${place_id}&key=${process.env.GOOGLE_API_KEY}`
+
+axios({
+  method: "get",
+  url: detilas_url,
+})
   .then((res) => {
-    res.data.results.map((place) => {
-      console.log("name ", place.name)
-      console.log("id ", place.place_id)
-      if (place.photos) {
-        place.photos.map((pic) => console.log(pic))
-      }
-      console.log("Geometry ", place.geometry)
-      console.log("\n==============================")
-    })
+    console.log(res.data)
   })
   .catch((er) => {
     console.log(er)
